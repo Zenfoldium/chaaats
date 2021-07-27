bcdb5dbd79ba334bb6cdba7b08c8879fec6dd758
Ashish Singh <82609788+Zenfoldium@users.noreply.github.com>
2021-07-27 21:56:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// const { socket } = require('socket.io');
+
+// Node server whuill will handle our Scocket.io
+const io = require('socket.io')(8000, {
+    cors: {
+      origin: '*',
+    }
+  });
+
+const user = {};
+
+io.on('connection',socket=>{
+    socket.on('new-user-joined', name =>{
+        console.log("new user is ", name);
+        user[socket.id]=name;
+        socket.broadcast.emit('user-joined',name);
+    });
+
+    socket.on('send',message =>{
+        socket.broadcast.emit('recieve',{message:message,name:user[socket.id]})
+    });
+
+    socket.on('disconnect',message=>{
+        socket.broadcast.emit('left',user[socket.id]);
+        delete user[socket.id];
+    })
+})
